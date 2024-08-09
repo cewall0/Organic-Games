3c6c74a1c530bf8ca7cf54e638821983a92c55f9
Chad Wallace <cewall0@gmail.com>
2024-08-08 23:04:35 -0400

    Initial Commit

---
+++ Organic Games/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Organic Games
+//
+//  Created by Chad Wallace on 8/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Organic Games/Organic_GamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Organic_GamesApp.swift
+//  Organic Games
+//
+//  Created by Chad Wallace on 8/8/24.
+//
+
+import SwiftUI
+
+@main
+struct Organic_GamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
